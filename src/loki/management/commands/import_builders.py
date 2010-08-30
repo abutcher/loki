9449dfa698bfec19bb9ae2c4b13ba651676121b8
Dan Radez <dradez@redhat.com>
2010-08-30 16:06:32 -0400

    adding an --all option for import builders

--- src/loki/management/commands/import_builders.py
+++ src/loki/management/commands/import_builders.py
@@ -35,6 +35,11 @@ class Command(BaseCommand):
             dest='auto',
             default=False,
             help='Assume yes to all y/n prompts'),
+        make_option('--all',
+            action="store_true",
+            dest='all',
+            default=False,
+            help='Import all builders'),
         )
 
     def yes_no(self, msg):
@@ -54,70 +59,75 @@ class Command(BaseCommand):
         for i in m_cfg['builders']:
             builders[i['name']] = i
 
-        if options['builder']:
-            builder = options['builder']
+        if options['all']:
+            all_builders = options['all']
+            import_builders = builders
+        elif options['builder']:
+            builders = [options['builder']]
         else:
             # ask what builder to import
             print "Builders:"
             for i in builders:
                 print ' - %s' % i
             builder = raw_input("\nWhich builder would you like to import? ")
+            import_builders = [builder]
 
-        # verify steps
-        print '\nVerifying Steps for builder %s:' % builder
-        x = 1
-        for ct, i in enumerate(builders[builder]['factory'].steps):
-            print '%s: %s' % (ct+1, str(i[0]))
-            for o in i[1].items():
-                print '    %s: %s' % o
+        for builder in import_builders:
+            # verify steps
+            print '\nVerifying Steps for builder %s:' % builder
+            x = 1
+            for ct, i in enumerate(builders[builder]['factory'].steps):
+                print '%s: %s' % (ct+1, str(i[0]))
+                for o in i[1].items():
+                    print '    %s: %s' % o
 
-        if not options['auto'] and not self.yes_no("Import these steps?"):
-            exit(0)
+            if not options['auto'] and not self.yes_no("Import these steps?"):
+                exit(0)
 
-        print '\nVerifying that step objects are registered:'
-        step_types = {}
-        for i in builders[builder]['factory'].steps:
-            try:
-                if str(i[0]) not in step_types:
-                    step_types[str(i[0])] = Config.objects.get(module=str(i[0]),
-                                                    content_type=step_content_type)
-                print ' - %s: registered' % str(i[0])
-            except:
-                print '\n%s is not registered with loki\n' % str(i[0])
-                if options['auto'] or self.yes_no("Import %s definition?" % str(i[0])):
-                    config_importer(str(i[0]), 'steps')
+            print '\nVerifying that step objects are registered:'
+            step_types = {}
+            for i in builders[builder]['factory'].steps:
+                try:
                     if str(i[0]) not in step_types:
                         step_types[str(i[0])] = Config.objects.get(module=str(i[0]),
                                                         content_type=step_content_type)
-                    print ' - %s: imported' % str(i[0])
-                else:
-                    print '\nCannot continue without registering \
-%s definition with loki\n' % str(i[0])
-                    exit(1)
-
-        # do the builder import
-        print '\nImporting builder %s' % builder
-        master = Master.objects.get(name=options['master']) 
-        builder = Builder(name=builder, master=master)
-        builder.save()
-        try:
-            builder.slaves = master.slaves.all()
-            builder.save()
-            for i, s in enumerate(builders[builder.name]['factory'].steps):
-                step = Step(builder=builder, type=step_types[str(s[0])], num=i+1)
-                step.save()
-                for p in step_types[str(s[0])].params.all():
-                    if p.name in s[1]:
-                        param = StepParam(step=step, type=p, val=pickle.dumps(s[1][p.name]),
-                              default=(s[1][p.name]==p.loads_default()))
+                    print ' - %s: registered' % str(i[0])
+                except:
+                    print '\n%s is not registered with loki\n' % str(i[0])
+                    if options['auto'] or self.yes_no("Import %s definition?" % str(i[0])):
+                        config_importer(str(i[0]), 'steps')
+                        if str(i[0]) not in step_types:
+                            step_types[str(i[0])] = Config.objects.get(module=str(i[0]),
+                                                            content_type=step_content_type)
+                        print ' - %s: imported' % str(i[0])
                     else:
-                        print '%s %s not defined in config being imported, using default: %s' % \
-                              (s[0], p.name, p.loads_default())
-                        param = StepParam(step=step, type=p, val=p.default, default=True)
-                    param.save() 
-            print '\nImport Complete\n\n'
-        except Exception, e:
-            #something failed so let's not leave the builder half imported
-            builder.delete()
-            print '\nImport Failed!'
-            raise e
+                        print '\nCannot continue without registering \
+    %s definition with loki\n' % str(i[0])
+                        exit(1)
+
+            # do the builder import
+            print '\nImporting builder %s' % builder
+            master = Master.objects.get(name=options['master']) 
+            builder = Builder(name=builder, master=master)
+            builder.save()
+            try:
+                builder.slaves = master.slaves.all()
+                builder.save()
+                for i, s in enumerate(builders[builder.name]['factory'].steps):
+                    step = Step(builder=builder, type=step_types[str(s[0])], num=i+1)
+                    step.save()
+                    for p in step_types[str(s[0])].params.all():
+                        if p.name in s[1]:
+                            param = StepParam(step=step, type=p, val=pickle.dumps(s[1][p.name]),
+                                  default=(s[1][p.name]==p.loads_default()))
+                        else:
+                            print '%s %s not defined in config being imported, using default: %s' % \
+                                  (s[0], p.name, p.loads_default())
+                            param = StepParam(step=step, type=p, val=p.default, default=True)
+                        param.save() 
+                print '\nImport Complete\n\n'
+            except Exception, e:
+                #something failed so let's not leave the builder half imported
+                builder.delete()
+                print '\nImport Failed!'
+                raise e
